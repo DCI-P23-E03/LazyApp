f41a3db6d2a291e61d529085003eb2be269d16d7
RubinaBallach <rubina.ballach@gmail.com>
2023-08-11 09:37:57 +0200

    new AI implementations with classes

---
+++ ai_example1_Class.py
@@ -0,0 +1,48 @@
+import openai
+
+# api key again
+key = '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'
+
+class ChatGPT:
+    def __init__(self, api_key=key, engine="text-davinci-003", max_tokens=150, temperature=0.7, completions=3, best_of=3, presence_penalty=0.5, frequency_penalty=0.5):
+        self.api_key = api_key
+        self.engine = engine # engine is the chatgpt model used, davinci is the smartest, fastest yet most expensive
+        self.max_tokens = max_tokens # maximum tokens allowed for response length
+        self.temperature = temperature #  0.0 is deterministic, completely strict to system role - 1.0 is creative, completely random responses (now choose)
+        self.completions = completions # number of completions per prompt
+        self.best_of = best_of # multiplies the number of completions and chooses the best of (multiplies costs too)
+        self.presence_penalty = presence_penalty # the higher the value, the more likely new topics will be introduced
+        self.frequency_penalty = frequency_penalty # the higher the value, the more likely information will be repeated
+        openai.api_key = self.api_key
+
+    def get_chatgpt_response(self, user_input):
+        response = openai.Completion.create(
+            engine=self.engine, # engine/model
+            prompt=user_input, # prompt for the ai
+            max_tokens=self.max_tokens, # maximum tokens allowed
+            temperature=self.temperature, # temperature (creativeness/strictness)
+            n=self.completions, # completion number
+            best_of=self.best_of, # best of responses
+            echo=False, # echo includes the user input (prompt) in the response
+            presence_penalty=self.presence_penalty, # chosen new-topics-probability
+            frequency_penalty=self.frequency_penalty # chosen repeat-info-probability
+        )
+        choices = [choice.text.strip() for choice in response.choices] # getting all responses from response
+        return choices
+
+    def chat_interface(self):
+        print("Welcome to ChatGPT!")
+        print("Type 'quit' to exit the chat.\n")
+        while True:
+            user_input = input("You: ")
+            if user_input.lower() == 'quit':
+                break
+            responses = self.get_chatgpt_response(user_input)
+            print("CHAT GPT: ")
+            for response in responses:
+                print(response)
+
+
+if __name__ == '__main__':
+    chat_gpt = ChatGPT() 
+    chat_gpt.chat_interface()
---
+++ ai_example2_Class.py
@@ -0,0 +1,50 @@
+import openai
+from stringcolor import *
+
+# Define the API key
+key = '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'
+
+class ChatGPT:
+    def __init__(self, api_key=key, model="gpt-3.5-turbo", max_tokens=150, temperature=0.7, completions=1, presence_penalty=0.5, frequency_penalty=0.5):
+        self.api_key = api_key
+        self.model = model # The ChatGPT model used (gpt-3.5-turbo is an example, can be replaced)
+        self.max_tokens = max_tokens # Maximum tokens allowed for response length
+        self.temperature = temperature # 0.0 is deterministic, 1.0 is creative
+        self.completions = completions # Number of completions per prompt
+        self.presence_penalty = presence_penalty # The higher the value, the more likely new topics will be introduced
+        self.frequency_penalty = frequency_penalty # The higher the value, the more likely information will be repeated
+        openai.api_key = self.api_key
+
+    def get_chatgpt_response(self, messages):
+        response = openai.ChatCompletion.create(
+            model=self.model, # ChatGPT model
+            messages=messages, # Ongoing conversation
+            temperature=self.temperature, # Chosen temperature (creativity/strictness)
+            max_tokens=self.max_tokens, # Maximum tokens allowed
+            n=self.completions, # Number of completions
+            presence_penalty=self.presence_penalty, # Chosen new-topics-probability
+            frequency_penalty=self.frequency_penalty # Chosen repeat-info-probability
+        )
+        choices = [choice.message['content'] for choice in response.choices] # Extracting the content of responses
+        return choices
+
+    def chat_interface(self):
+        # Display welcome message
+        print(cs("Welcome to ChatGPT!", "blue"))
+        print(cs("Type 'quit' to exit the chat.\n", "darkblue"))
+        system_role = "You are a helpful assistant" # DEFINE SYSTEM ROLE HERE
+        messages = [{"role": "system", "content": system_role}] 
+        while True:
+            user_input = input("You: ")
+            if user_input.lower() == 'quit':
+                break
+            messages.append({"role": "user", "content": user_input}) # Append user's input to messages
+            responses = self.get_chatgpt_response(messages) # Get responses from ChatGPT
+            print("CHAT GPT: ")
+            for resp in responses:
+                print(cs(resp, "green")) # Display AI's response in green
+                messages.append({"role": "assistant", "content": resp}) # Append AI's response to messages
+
+if __name__ == '__main__':
+    chat_gpt = ChatGPT() 
+    chat_gpt.chat_interface()
