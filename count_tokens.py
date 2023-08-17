66986bdf8929b497cd1b0b21335e234d27952614
crstns <schneeweiss.carsten@gmail.com>
2023-08-17 13:51:57 +0200

    added the count_token.py-file

---
+++ count_tokens.py
@@ -0,0 +1,11 @@
+import tiktoken
+
+def count_tokens(text):
+            #enc = tiktoken.encoding_for_model("text-davinci-003")
+            enc = tiktoken.encoding_for_model("gpt-4")
+            tokens = len(enc.encode(text))
+            tokens_left = 4096 - tokens
+            return tokens, tokens_left
+    
+print("Tokens used:", count_tokens(str(conversation))[0])
+print("Tokens left:", count_tokens(str(conversation))[1])
