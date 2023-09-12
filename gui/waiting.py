f2ee38ef5a603293a0970ee7862f602f4db12a6d
RubinaBallach <rubina.ballach@gmail.com>
2023-09-12 10:11:59 +0200

    Jokes implemented for waiting box

--- gui/waiting.py
+++ gui/waiting.py
@@ -1,24 +1,31 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QMessageBox
-from PyQt6.QtGui import QMovie, QPixmap
-from PyQt6 import QtCore
+import pyjokes
+#from PyQt6.QtGui import QMovie, QPixmap
+#from PyQt6 import QtCore
 
 def msg_wait(language):
     global msg
     msg = QMessageBox()
     msg.setGeometry(200,200,200,200)
-    #msg.setIconPixmap(QPixmap('gui/loading_cat.gif').scaledToWidth(550))
+
+    # picture option that was not working smoothly
+    #   msg.setIconPixmap(QPixmap('gui/loading_cat.gif').scaledToWidth(550))
     
-   # icon_label = msg.findChild(QLabel, "qt_msgboxex_icon_label")
-    #movie = QMovie('gui/loading_cat.gif')
-    #setattr(msg, 'icon_label', movie)
-    #icon_label.setMovie(movie)
-    #movie.start()
+    #   icon_label = msg.findChild(QLabel, "qt_msgboxex_icon_label")
+    #   movie = QMovie('gui/loading_cat.gif')
+    #   setattr(msg, 'icon_label', movie)
+    #i  con_label.setMovie(movie)
+    #   movie.start()
+
+    
     if language == "de":
-        msg.setText("Die KI arbeitet, ein bisschen Geduld bitte.")
+        joke = pyjokes.get_joke("de", "all")
+        msg.setText(joke)
         msg.setWindowTitle("Nur eine Minute...")
     else:
-        msg.setText("AI is working really hard on your request")
+        joke = pyjokes.get_joke("en", "all")
+        msg.setText(joke)
         msg.setWindowTitle("Just a minute...")
     msg.setModal(False)
     msg.show()
@@ -28,5 +35,5 @@ def msg_close():
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    msg_wait("de")
+    msg_wait("en")
     sys.exit(app.exec())
